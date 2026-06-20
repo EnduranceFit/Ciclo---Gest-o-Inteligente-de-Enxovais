760d8be24fc2ad3166f6502788c61da93e90dc11
Jonatan Alves <luthierjonatanalves@gmail.com>
2026-06-20 17:23:45 -0300

    Add debug sql

---
+++ api/debug-sql.ts
@@ -0,0 +1,8 @@
+export default async function handler(req: any, res: any) {
+  try {
+    const { sql } = await import('@vercel/postgres');
+    res.status(200).json({ status: 'ok', sql: typeof sql });
+  } catch (e: any) {
+    res.status(500).json({ error: e.message, stack: e.stack });
+  }
+}
